044f0572dbee5463a86a877fa122db35ceed63b2
Jade Chang <jade.chang@thecarousell.com>
2025-10-17 00:32:55 -0400

    update script

--- Lab 4/lab4_script.py
+++ Lab 4/lab4_script.py
@@ -1,39 +1,224 @@
-def display(image):
-    '''display <image> on piscreen'''
+from digitalio import DigitalInOut
+import busio
+import board
+import time
+from PIL import Image, ImageDraw
+import adafruit_rgb_display.ili9341 as ili9341
+import adafruit_rgb_display.st7789 as st7789  # pylint: disable=unused-import
+import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
+import adafruit_rgb_display.st7735 as st7735  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1351 as ssd1351  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1331 as ssd1331  # pylint: disable=unused-import
+import adafruit_ssd1306
+import qwiic
+import adafruit_mpr121
+from adafruit_apds9960.apds9960 import APDS9960
+from adafruit_apds9960 import colorutility
+from adafruit_seesaw import seesaw, rotaryio, digitalio
+import qwiic_joystick
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = DigitalInOut(board.D5)
+dc_pin = DigitalInOut(board.D25)
+reset_pin = DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# pylint: disable=line-too-long
+# Create the display:
+# disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
+# disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
+# disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
+# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+# disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
+# disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+# disp = st7735.ST7735R(spi, rotation=90, bgr=True,                 # 0.96" MiniTFT ST7735R
+# disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
+# disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
+# disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+# pylint: enable=line-too-long
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp.rotation % 180 == 90:
+    height = disp.width  # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width  # we swap height/width to rotate it to landscape!
+    height = disp.height
+image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image)
+
+# prox setup
+ToF = qwiic.QwiicVL53L1X()
+
+# light setup
+i2c = busio.I2C(board.SCL, board.SDA)
+apds = APDS9960(i2c)
+apds.enable_color = True
+
+# joystick setup
+myJoystick = qwiic_joystick.QwiicJoystick(i2c)
+myJoystick.begin()
+
+# rot setup
+seesaw = seesaw.Seesaw(i2c, addr=0x36)
+
+# oled setup
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+# start with a blank screen
+oled.fill(0)
+oled.show()
+
+seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
+
+seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
+button = digitalio.DigitalIO(seesaw, 24)
+button_held = False
+
+encoder = rotaryio.IncrementalEncoder(seesaw)
+last_position = -encoder.position
+
+# initialize capacity twizzer
+mpr121 = adafruit_mpr121.MPR121(i2c, address=0x5A)
+
+def display(image_name):
+    image = Image.open(image_name)
+    backlight = DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = True
 
 
-def oled(hp):
-    '''show <hp>, maybe with a red bar or whatever is convenient to you'''
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
+
+def draw_hp_bar(hp: int, max_hp: int = 5):
+    """
+    Draws an HP bar with 'hp' filled blocks and 'max_hp' total blocks.
+    Each block is a filled rectangle separated by a dark line.
+    """
+    oled.fill(0)  # clear screen first
+
+    # Draw "HP" text
+    oled.text("HP", 0, 10, 1)
+
+    # Define block layout
+    block_width = 18  # width of each HP block
+    block_height = 10  # height of each HP block
+    spacing = 2  # space between blocks
+    start_x = 30  # where to start drawing blocks (after "HP")
+    start_y = 10
+
+    # Draw filled HP blocks
+    for i in range(max_hp):
+        x = start_x + i * (block_width + spacing)
+        if i < hp:
+            oled.fill_rect(x, start_y, block_width, block_height, 1)  # filled
+        else:
+            oled.rect(x, start_y, block_width, block_height, 1)  # empty outline
+
+    oled.show()
+
+def display_text(text):
+    """
+    Display a single line of text on the OLED.
+    """
+    oled.fill(0)           # clear screen
+    oled.text(text, 0, 10, 1)  # draw text at position (x=0, y=10)
+    oled.show()            # update display
 
 def joystick():
-    '''read x, y as the Lab4 example. Return True if |x-512| + |y-512| >= 200 (or other threshold, to be determined). False if not'''
-
+    # if |x - 512| + |y - 512| > 200 return True
+    myJoystick.read()
+    x = myJoystick.horizontal
+    y = myJoystick.vertical
+    if abs(x - 512) + abs(y - 512) > 200:
+        return True
+    else:
+        return False
 
 def prox_sensor():
-    '''read prox as the Lab4 example. Return True if prox <= 100 (or other threshold, to be determined). False if not'''
-
+    ToF.sensor_init()
+    ToF.start_ranging()
+    while not ToF.data_ready():
+        time.sleep(0.5)
+        pass
+    distance = ToF.get_distance()
+    ToF.clear_interrupt()
+    ToF.stop_ranging()
+    if distance > 200:
+        return True
+    else:
+        return False
 
 def light_sensor():
-    '''read dark as the Lab4 example. Return True if dark >= 225 (or other threshold, to be determined). False if not'''
-
+    # wait for color data to be ready
+    while not apds.color_data_ready:
+        time.sleep(0.005)
+    r, g, b, c = apds.color_data
+    if c <= 100:
+        return True
+    else:
+        return False
 
 def rotary():
-    '''read rot as the Lab4 example. return True if rot >= 12 (or other threshold, to be determined). False if not'''
+    # negate the position to make clockwise rotation positive
+    global last_position
+    position = -encoder.position
+    diff = abs(last_position - position)
+    last_position = position
+    if diff >= 5:
+        return True
+    else:
+        return False
 
 
-'''there will be many initializing codes here'''
-# initialize capacity twizzer
-i2c = busio.I2C(board.SCL, board.SDA)
-mpr121 = adafruit_mpr121.MPR121(i2c)
-# max hp here
-hp = 5
+trigger = False
+hp=5
 while True:
-    oled(hp)
+    draw_hp_bar(hp)
 
     # 0 = lightning, 1= fireball, 2 = earthquake, 3 = slime
     if mpr121[0].value:
-        display("lightning.jpg")
+        display_text("INCOMING")
+        display("lightning.png")
         start_time = time.time()
         trigger = False
         while time.time() - start_time < 2:
@@ -42,9 +227,18 @@ while True:
                 break
             if light_sensor():
                 trigger = True
+                display("victory.png")
                 break
+        if not trigger:
+            hp -= 1
+            draw_hp_bar(hp)
+            display_text("STRUCK")
+        else:
+            display_text("SAFE.")
+
     if mpr121[1].value:
-        display("fireball.jpg")
+        display_text("INCOMING")
+        display("fireball.png")
         start_time = time.time()
         trigger = False
         while time.time() - start_time < 2:
@@ -53,9 +247,18 @@ while True:
                 break
             if joystick():
                 trigger = True
+                display("victory.png")
                 break
+        if not trigger:
+            hp -= 1
+            draw_hp_bar(hp)
+            display_text("BURNED")
+        else:
+            display_text("SAFE.")
+
     if mpr121[2].value:
-        display("earthquake.jpg")
+        display_text("INCOMING")
+        display("earthquake.png")
         start_time = time.time()
         trigger = False
         while time.time() - start_time < 2:
@@ -64,9 +267,18 @@ while True:
                 break
             if prox_sensor():
                 trigger = True
+                display("victory.png")
+                display_text("CRUSHED")
                 break
+        if not trigger:
+            hp -= 1
+            draw_hp_bar(hp)
+        else:
+            display_text("SAFE.")
+
     if mpr121[3].value:
-        display("slime.jpg")
+        display_text("INCOMING")
+        display("slime.png")
         start_time = time.time()
         trigger = False
         while time.time() - start_time < 2:
@@ -75,10 +287,17 @@ while True:
                 break
             if rotary():
                 trigger = True
+                display("victory.png")
+                display_text("SLIMED")
                 break
 
         if not trigger:
             hp -= 1
+            draw_hp_bar(hp)
+        else:
+            display_text("SAFE.")
 
-        if hp == 0:
-            display("skeleton.jpg")
+    if hp == 0:
+            display("skeleton.png")
+
+    print(hp)
