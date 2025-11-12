3edb5e0bfc02cd190e33f63dc580c6722f9e06de
Jade Chang <jade.chang@thecarousell.com>
2025-11-12 15:48:45 -0500

    add battle ships

---
+++ Lab 6/battle_ship/main.py
@@ -0,0 +1,256 @@
+import pandas as pd
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+import busio
+import adafruit_rgb_display.ili9341 as ili9341
+import adafruit_rgb_display.st7789 as st7789  # pylint: disable=unused-import
+import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
+import adafruit_rgb_display.st7735 as st7735  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1351 as ssd1351  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1331 as ssd1331  # pylint: disable=unused-import
+
+import adafruit_mpr121
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.D5)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 22)
+
+
+def wait_for_touch():
+    print("Touch a pad to choose ROW...")
+    row_chosen = False
+    while not row_chosen:
+        for i in range(3):
+            if mpr121[i].value:
+                if i == 0:
+                    cell = "A"
+                elif i == 1:
+                    cell = "B"
+                elif i == 2:
+                    cell = "C"
+                print(f"Row selected: {cell}")
+                row_chosen = True
+                while mpr121[i].value:
+                    time.sleep(0.2)
+                break
+        time.sleep(0.05)
+
+    print("Touch a pad to choose COLUMN...")
+    col_chosen = False
+    while not col_chosen:
+        for i in range(4):
+            if mpr121[i].value:
+                cell += str(i + 1)
+                print(f"Column selected: {i + 1}")
+                col_chosen = True
+                while mpr121[i].value:
+                    time.sleep(0.2)
+                break
+        time.sleep(0.05)
+
+    print(f"? Selected cell: {cell}")
+    return cell
+
+
+def draw_square(C, side):
+    cx = point_dict[C][0]
+    cy = point_dict[C][1]
+    x0 = cx - side / 2
+    y0 = cy - side / 2
+    x1 = cx + side / 2
+    y1 = cy + side / 2
+    draw.rectangle((x0, y0, x1, y1), outline="white", width=3)
+    disp.image(image, rotation=90)
+
+
+def draw_cross(C, side):
+    cx = point_dict[C][0]
+    cy = point_dict[C][1]
+    x0 = cx - side / 2
+    y0 = cy - side / 2
+    x1 = cx + side / 2
+    y1 = cy + side / 2
+    draw.line((x0, y0, x1, y1), fill="white", width=3)
+    draw.line((x0, y1, x1, y0), fill="white", width=3)
+    disp.image(image, rotation=90)
+
+
+def draw_word(ship, C):
+    cx = point_dict[C][0]
+    cy = point_dict[C][1]
+    if ship == "battleship":
+        s = "B"
+    if ship == "destroyer":
+        s = "D"
+    if ship == "submarine":
+        s = "S"
+    s_bbox = draw.textbbox((0, 0), s, font=font)
+    s_width = s_bbox[2] - s_bbox[0]
+    s_height = s_bbox[3] - s_bbox[0]
+    draw.text((cx - s_width // 2, cy - s_height // 2), s, font=font, fill="white")
+    disp.image(image, rotation=90)
+
+
+def load_ships(csv_path="ships.csv"):
+    df = pd.read_csv(csv_path)
+    ships = {}
+    for _, row in df.iterrows():
+        ship_type = row["type"]
+        cells = [c.strip().upper() for c in row["cells"].split(",")]
+        ships[ship_type] = cells
+    return ships
+
+
+def check_hit(cell, ships):
+    for ship, cells in ships.items():
+        if cell in cells:
+            cells.remove(cell)
+            print(f"Hit {ship}!")
+            if len(cells) == 0:
+                print(f"Sink {ship}!")
+            return True, ship
+    print("Miss!")
+    return False, None
+
+
+def display(image_name):
+    image = Image.open(image_name)
+    image = image.rotate(270, expand=True)
+    backlight = digitalio.DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = True
+
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image, rotation=90)
+
+
+# main loop
+ships = load_ships("ships.csv")  # !!! to build a map for now, comment it when server is implemented
+
+'''
+myships = load_ships("ships.csv")
+send_map(myships)
+ships = read_map()   # opponent map
+'''
+
+print("Ship map loaded")
+for k, v in ships.items():
+    print(f"  {k}: {v}")
+
+turn = True
+all_cells = sum(len(v) for v in ships.values())
+
+point_dict = {'A1': [60, 25],
+              'A2': [100, 25],
+              'A3': [140, 25],
+              'A4': [180, 25],
+              'B1': [60, 65],
+              'B2': [100, 65],
+              'B3': [140, 65],
+              'B4': [180, 65],
+              'C1': [60, 105],
+              'C2': [100, 105],
+              'C3': [140, 105],
+              'C4': [180, 105]}
+
+draw_square('A1', 40)
+draw_square('A2', 40)
+draw_square('A3', 40)
+draw_square('A4', 40)
+draw_square('B1', 40)
+draw_square('B2', 40)
+draw_square('B3', 40)
+draw_square('B4', 40)
+draw_square('C1', 40)
+draw_square('C2', 40)
+draw_square('C3', 40)
+draw_square('C4', 40)
+
+gameplay = True
+while gameplay:
+    '''
+    turn = read_turn()
+    status = read_status()
+    if status == "lose":
+        print("You lose!")
+        display("skeleton.png")
+        gameplay = False
+        '''
+    turn = True  # to make the game continue for now, comment it if server implemrnted
+    print("Another try...")
+    while turn:
+        guess = wait_for_touch()
+        if not guess:
+            continue
+
+        hit, ship = check_hit(guess, ships)
+        if hit:
+            turn = True
+            draw_word(ship, guess)
+            all_cells = sum(len(v) for v in ships.values())
+            if all_cells == 0:
+                print("You win!")
+                display("victory.png")
+                '''send_status("end_game")'''
+                gameplay = False
+                break
+        else:
+            turn = False
+            draw_cross(guess, 40)
+            '''send_turn("opponent")'''
